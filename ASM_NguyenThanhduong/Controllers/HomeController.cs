e4af5a977cc2661ad6ee5482aa6d9b9954c17d6c
NTDuong2405 <ntduonga15568@gmail.com>
2021-03-15 19:49:20 +0700

    ASM

---
+++ ASM_NguyenThanhduong/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ASM_NguyenThanhduong.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASM_NguyenThanhduong.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext _db = new MyDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(User user)
+        {
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+     
+    }
+}
---
+++ ASM_NguyenThanhduong/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASM_NguyenThanhduong.Models
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext() : base("name = SMSProjectConnectionString")
+        {
+
+        }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
